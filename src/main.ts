6cad974c11a768cb8fe43299b110dd1f390a64ed
elaurentium <evan.limeira@gmail.com>
2025-07-31 20:01:26 -0300

    feat: docker rodando e swagger configurado

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Rei do Acolchoado')
+    .setDescription('The Rei do Acolchoado API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); // swagger vai aparecer na rota /docs
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

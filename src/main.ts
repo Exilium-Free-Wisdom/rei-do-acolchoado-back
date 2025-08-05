36ae320b9715b8f33d0dfcec5699554c8d405fbf
elaurentium <evan.limeira@gmail.com>
2025-08-04 22:22:17 -0300

    feat: melhorando o log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { app } from "./app";
 
-
 const port = Number(process.env.PORT) || 3000;
-    
-app.listen(PORT => {
-    console.log(`Server running on port ${port}`)
+
+app.listen(port, '0.0.0.0').then((address) => {
+  console.log(`Server listening on ${address}`);
+}).catch((err) => {
+  console.error(err);
+  process.exit(1);
 })

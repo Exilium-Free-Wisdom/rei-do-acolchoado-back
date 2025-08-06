efe8c7d3ac3814075aa1fb1187c6d11215d839ac
elaurentium <evan.limeira@gmail.com>
2025-08-05 22:37:53 -0300

    feat: passando a porta e host

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { app } from "./app";
 
-const port = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(port, '0.0.0.0').then((address) => {
+app.listen({ port: PORT, host: '0.0.0.0' }).then((address) => {
   console.log(`Server listening on ${address}`);
 }).catch((err) => {
   console.error(err);
